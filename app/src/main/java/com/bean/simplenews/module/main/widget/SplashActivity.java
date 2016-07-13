fb506bebeb1633a5f9a8e18c3e070a264b662df4
binzhihao <binzhihao@outlook.com>
2016-07-14 00:22:52 +0800

    fix bug

--- app/src/main/java/com/bean/simplenews/module/main/widget/SplashActivity.java
+++ app/src/main/java/com/bean/simplenews/module/main/widget/SplashActivity.java
@@ -18,6 +18,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
             }
         },2000);
     }
